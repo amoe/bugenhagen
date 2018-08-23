a9a03533bcdaf412b72cac75d7bc6422aff6e97a
David Banks <amoebae@gmail.com>
2018-08-23 21:17:42 +0100

    expanding all articles

--- create_xhtml.py
+++ create_xhtml.py
@@ -3,6 +3,7 @@ import pprint
 import jinja2
 import os
 import pathlib
+import shutil
 
 env = jinja2.Environment(
     loader=jinja2.FileSystemLoader('templates'),
@@ -21,6 +22,8 @@ pathlib.Path(output_dir).mkdir(exist_ok=True)
 
 print("Rendering template")
 
+compiled_articles = []
+
 for index, record in enumerate(articles):
     if record['user'] != 'NUKLEAR-SLUG':
         continue
@@ -34,23 +37,41 @@ for index, record in enumerate(articles):
 
     result = tmpl.render(tmpl_params)
 
-    output_path = os.path.join(output_dir, "article-%.4d.xhtml" % index)
+    article_basename = "article-%.4d.xhtml" % index
+    output_path = os.path.join(output_dir, article_basename)
 
     with open(output_path, 'w') as f:
         f.write(result)
+
+    compilation_record = {
+        'basename': article_basename,
+        'article_path': output_path
+    }
+
+    compiled_articles.append(compilation_record)
 
 print("Render done")
 
 epub_root_dir = "epub_out"
 pathlib.Path(epub_root_dir).mkdir(exist_ok=True)
 
-def epub_root_render(source, target):
-    content = env.get_template(source).render({})
+def epub_root_render(source, target, context={}):
+    content = env.get_template(source).render(context)
     path = os.path.join(epub_root_dir, target)
     with open(path, 'w') as f:
         f.write(content)
 
+
+article_subdirectory = 'articles'
+
+article_target_dir = os.path.join(epub_root_dir, article_subdirectory)
+pathlib.Path(article_target_dir).mkdir(exist_ok=True)
+
+
 epub_root_render('main.opf.j2', 'main.opf')
 epub_root_render('mimetype.j2', 'mimetype')
-epub_root_render('nav.xhtml.j2', 'nav.xhtml')
+epub_root_render('nav.xhtml.j2', 'nav.xhtml', {'articles': compiled_articles,
+                                               'article_subdirectory': article_subdirectory})
 
+for article in compiled_articles:
+    shutil.copy(article['article_path'], article_target_dir)
