b5a65b72aee18db853808167a08f31ff9c298f4d
David Banks <amoebae@gmail.com>
2018-08-23 20:16:30 +0100

    expanding template

--- create_xhtml.py
+++ create_xhtml.py
@@ -1,6 +1,8 @@
 import pickle
 import pprint
 import jinja2
+import os
+import pathlib
 
 env = jinja2.Environment(
     loader=jinja2.FileSystemLoader('templates'),
@@ -14,15 +16,23 @@ with open('foo.pkl', 'rb') as handle:
 tmpl = env.get_template('mytemplate.xhtml.j2')
 
 
-for record in articles:
-    print(record['user'])
+output_dir = "expanded"
+pathlib.Path(output_dir).mkdir(exist_ok=True) 
+
+for index, record in enumerate(articles):
+    if record['user'] != 'NUKLEAR-SLUG':
+        continue
+
     body = record['body']
     
     tmpl_params = {
-        'title': "foo bar",
+        'title': "Article %d" % index,
         'body': body
     }
 
     result = tmpl.render(tmpl_params)
-#    print(result)
 
+    output_path = os.path.join(output_dir, "article-%.4d.xhtml" % index)
+
+    with open(output_path, 'w') as f:
+        f.write(result)
