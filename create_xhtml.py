c4ddb6e8c2301b792223e9104890ab55981d8e04
David Banks <amoebae@gmail.com>
2018-08-23 20:19:53 +0100

    rendering opf template

--- create_xhtml.py
+++ create_xhtml.py
@@ -19,6 +19,8 @@ tmpl = env.get_template('mytemplate.xhtml.j2')
 output_dir = "expanded"
 pathlib.Path(output_dir).mkdir(exist_ok=True) 
 
+print("Rendering template")
+
 for index, record in enumerate(articles):
     if record['user'] != 'NUKLEAR-SLUG':
         continue
@@ -36,3 +38,13 @@ for index, record in enumerate(articles):
 
     with open(output_path, 'w') as f:
         f.write(result)
+
+print("Render done")
+
+epub_root_dir = "epub_out"
+pathlib.Path(epub_root_dir).mkdir(exist_ok=True)
+
+opf_content = env.get_template('main.opf.j2').render({})
+opf_path = os.path.join(epub_root_dir, 'main.opf')
+with open(opf_path, 'w') as f:
+    f.write(opf_content)
