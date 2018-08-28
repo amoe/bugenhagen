35b29750c07bf23693a999c7deff81e3171fac78
David Banks <amoebae@gmail.com>
2018-08-29 00:47:35 +0100

    starting to create compliant epub archive

--- archiver.py
+++ archiver.py
@@ -1,4 +1,5 @@
 import zipfile
+import os.path
 
 def epubzip(path):
     return zipfile.ZipFile(
@@ -6,7 +7,12 @@ def epubzip(path):
     )
 
 
-with epubzip('out.zip') as z:
-    z.write('templates/mimetype.j2', arcname='mimetype')
-    
+base_epub_dir = "epub_skeleton/content"
 
+def do_archive(zf, base_dir):
+    mimetype_full_path = os.path.join(base_epub_dir, 'mimetype')
+
+    zf.write(mimetype_full_path, arcname='mimetype')
+
+with epubzip('out.epub') as z:
+    do_archive(z, base_epub_dir)
