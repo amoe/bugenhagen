246d44987f0e2e74a7b8471b44c6733a3445f81a
David Banks <amoebae@gmail.com>
2018-09-01 02:35:25 +0100

    investigating document structure

--- odell.py
+++ odell.py
@@ -1,5 +1,6 @@
 import docx
 import pprint
+import random
 
 # python-docx 0.8.7
 
@@ -8,7 +9,7 @@ PATH = "/home/amoe/martin_odell/Object-Oriented Methods a Foundation, Martin and
 doc = docx.Document(PATH)
 
 
-# public api is
+# public api for document is
 
  # 'add_heading',
  # 'add_page_break',
@@ -27,5 +28,33 @@ doc = docx.Document(PATH)
  # 'styles',
  # 'tables'
 
-for (index, paragraph) in enumerate(doc.paragraphs):
-    print(index)
+# for (index, paragraph) in enumerate(doc.paragraphs):
+#     print(index)
+
+all_paragraphs = doc.paragraphs
+# index = random.randint(0, len(all_paragraphs))
+index = 3636
+some_para = all_paragraphs[index]
+
+# API for  paragraph consists of 
+
+ # 'add_run',
+ # 'alignment',
+ # 'clear',
+ # 'insert_paragraph_before',
+ # 'paragraph_format',
+ # 'part',
+ # 'runs',
+ # 'style',
+ # 'text'
+
+print(index)
+print(some_para.text)
+
+for run in some_para.runs:
+    print(run.text)
+
+        # text = ''
+        # for run in self.runs:
+        #     text += run.text
+        # return text
